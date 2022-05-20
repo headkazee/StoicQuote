377ab33097119d3563ddb00ae243448236ee4c51
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-05-20 23:41:17 +0800

    Initial commit.

--- StoiQuote/ContentView.swift
+++ StoiQuote/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
+struct StoicData: Decodable {
+    var data: Data
+}
+
+struct Data: Decodable {
+    var author: String
+    var quote: String
+}
+
+
 struct ContentView: View {
+    
+    @State var quote = StoicData(data: Data(author: "emelyn", quote: "eme"))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Text("\(quote.data.quote)")
+            Text("\(quote.data.author)")
+            Button("Next") {
+                self.fetchData()
+            }
+        }
+        .task {
+            self.fetchData()
+        }
+        
+    }
+    
+    func fetchData() {
+        // create url
+        let url = URL(string: "https://api.themotivate365.com/stoic-quote")
+        
+        URLSession.shared.dataTask(with: url!) {data, _, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(StoicData.self, from: data)
+                        self.quote = decodedData
+                    } catch {
+                        print("Something went wrong.")
+                    }
+                }
+            }
+        } .resume()
     }
 }
 
