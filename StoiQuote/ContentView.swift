8ff8b1aa8ca904b88f964b4fc90f789f595f7758
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-05-20 21:39:46 +0800

    Initial Commit

---
+++ StoiQuote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoiQuote
+//
+//  Created by Jayvee Ballesteros on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoiQuote/StoiQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoiQuoteApp.swift
+//  StoiQuote
+//
+//  Created by Jayvee Ballesteros on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct StoiQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
